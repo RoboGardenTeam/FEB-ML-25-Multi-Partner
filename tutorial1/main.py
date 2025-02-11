386d053236d59f6d8c9d76f77086ad74d788d48c
mostafa_microengineering <mostafa.moharram@microengineering.ca>
2025-02-10 19:25:59 -0500

    added tutorial 2 and 3

--- tutorial1/main.py
+++ tutorial1/main.py
@@ -2,9 +2,5 @@ def Hello(name):
     result = 'Hello' + name
     return result
 
-
 result = Hello('mostafa')
 print(Hello('mostafa'))
-first_number = 78
-second_number = 89
-#print(first_number, second_number)
